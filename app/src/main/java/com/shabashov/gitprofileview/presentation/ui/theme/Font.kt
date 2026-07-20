ca98ae96994579d21b4b9ae219317d31268338b1
Denis Shabashov <countenum404@gmail.com>
2026-07-20 20:59:25 +0300

    add Font.kt

---
+++ app/src/main/java/com/shabashov/gitprofileview/presentation/ui/theme/Font.kt
@@ -0,0 +1,12 @@
+package com.shabashov.gitprofileview.presentation.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.shabashov.gitprofileview.R
+
+val JetBrainsFontFamily = FontFamily(
+    Font(R.font.jet_brains_mono_bold, FontWeight.Bold),
+    Font(R.font.jet_brains_mono_thin, FontWeight.Thin),
+    Font(R.font.jet_brains_mono_medium, FontWeight.Medium),
+)
