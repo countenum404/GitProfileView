0bf64e475ce84f8d6dc46370e28083797b7150e9
Denis Shabashov <countenum404@gmail.com>
2026-08-22 03:42:46 +0300

    fix review comments

--- app/src/main/java/com/shabashov/gitprofileview/presentation/finder/SearchProfileView.kt
+++ app/src/main/java/com/shabashov/gitprofileview/presentation/finder/SearchProfileView.kt
@@ -1,7 +1,6 @@
 package com.shabashov.gitprofileview.presentation.finder
 
 import android.content.Intent
-import android.net.Uri
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -13,7 +12,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -35,22 +33,21 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.net.toUri
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.shabashov.gitprofileview.domain.Profile
 import com.shabashov.gitprofileview.presentation.ui.theme.JetBrainsFontFamily
-import androidx.core.net.toUri
 
 @Preview
 @Composable
 fun SearchProfileView(
-    modifier: Modifier = Modifier.fillMaxSize(),
+    modifier: Modifier = Modifier,
     viewModel: SearchProfileViewModel = viewModel()
 ) {
     val launcher = rememberLauncherForActivityResult(
@@ -193,7 +190,7 @@ fun ProfileCard(
             )
         ) {
             Text(
-                text = "${profile.name}",
+                text = profile.name,
                 fontSize = 24.sp,
                 fontWeight = FontWeight.Bold,
                 maxLines = 1,
@@ -201,7 +198,7 @@ fun ProfileCard(
                 fontFamily = JetBrainsFontFamily,
             )
             Text(
-                text = "${profile.fullName}",
+                text = profile.fullName,
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis,
                 fontFamily = JetBrainsFontFamily,
@@ -250,7 +247,7 @@ fun ProfileCard(
 @Composable
 private fun SubTitle(
     modifier: Modifier = Modifier,
-    text: String
+    text: String = ""
 ) {
     Text(
         modifier = modifier,
